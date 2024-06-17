002bd3d87ade157122c9901a5a35796a12d5e0fb
YASWANTH MADANAPALLI <71337774+yaswanthmadanapalli@users.noreply.github.com>
2024-06-17 15:28:02 -0700

    Time: 3 ms (29.51%), Space: 43.2 MB (6.88%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -0,0 +1,29 @@
+class MyComp implements Comparator<Integer>{
+    public int compare(Integer o1, Integer o2){
+        if(o1<o2){
+            return 1;
+        }
+        if(o1>o2){
+            return -1;
+        }
+        return 0;
+    }
+}
+class Solution {
+    
+    public int maxProduct(int[] nums) {
+        PriorityQueue<Integer> result=new PriorityQueue<>(new MyComp());
+        int[] temp=new int[2];
+        int i=0;
+        int result1=1;
+        for(int j=0;j<nums.length;j++){
+            result.add(nums[j]);
+        }
+        while(i<temp.length){
+            temp[i]=result.poll();
+            result1=result1*(temp[i]-1);
+            i++;
+        }
+        return result1;
+    }
+}
