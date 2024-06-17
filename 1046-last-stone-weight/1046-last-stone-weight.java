1c96f315a2b3376adefc440ecc13af863a990953
YASWANTH MADANAPALLI <71337774+yaswanthmadanapalli@users.noreply.github.com>
2024-06-17 16:57:49 -0700

    Time: 1 ms (98.75%), Space: 41.3 MB (10.82%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,16 +1,17 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> result=new PriorityQueue<>(new Comparator<Integer>(){
-           public int compare(Integer o1, Integer o2){
-               if(o1<o2){
-                   return 1;
-               }
-               if(o1>o2){
-                   return -1;
-               }
-               return 0;
-           } 
-        });
+        PriorityQueue<Integer> result=new PriorityQueue<>(Collections.reverseOrder());
+        // {
+        //    public int compare(Integer o1, Integer o2){
+        //        if(o1<o2){
+        //            return 1;
+        //        }
+        //        if(o1>o2){
+        //            return -1;
+        //        }
+        //        return 0;
+        //    } 
+        // });
         for(int i=0;i<stones.length;i++){
             result.add(stones[i]);
         }
