d4652a069c735276567a5caaa8d4c7bbc64e2b2a
YASWANTH MADANAPALLI <71337774+yaswanthmadanapalli@users.noreply.github.com>
2024-06-18 16:23:18 -0700

    Time: 11 ms (17.26%), Space: 44.2 MB (63.58%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+        int m=grid.length;
+        //int n=grid[0].length; won't work for input: [[10],[10,20]]
+        //PriorityQueue<Integer> result=new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer>[] result=new PriorityQueue[grid.length]; //Array of PriorityQueue syntax.
+        int total=0;
+        for(int i=0;i<grid.length;i++){
+            result[i]=new PriorityQueue<>(Comparator.reverseOrder());
+            for(int j=0;j<grid[i].length;j++){
+                result[i].add(grid[i][j]);
+            }
+        }
+        for(int column=0;column<grid[0].length;column++){
+            int maximumElement=Integer.MIN_VALUE;
+            for(int row=0;row<grid.length;row++){
+                int removedElement=result[row].poll();
+                maximumElement=Math.max(removedElement,maximumElement);
+            }
+            total+=maximumElement;
+        }
+        return total;
+        
+        
+    }
+}
