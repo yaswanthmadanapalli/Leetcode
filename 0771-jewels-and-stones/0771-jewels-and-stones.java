b96feab7734b02848a5bc124150e5bb42adef9f8
YASWANTH MADANAPALLI <71337774+yaswanthmadanapalli@users.noreply.github.com>
2024-06-21 09:09:09 -0700

    Time: 1 ms (60.63%), Space: 41.8 MB (21.90%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -4,12 +4,12 @@ class Solution {
         int total=0;
         for(int i=0;i<jewels.length();i++){
             char ch=jewels.charAt(i);
-            // if(hm.containsKey(ch)){
-            //     hm.put(ch,hm.get(ch)+1);
-            // }
-            // else{
+            if(hm.containsKey(ch)){
+                hm.put(ch,hm.get(ch)+1);
+            }
+            else{
                 hm.put(ch,1);
-            //}
+            }
         }
         for(int j=0;j<stones.length();j++){
             char ch=stones.charAt(j);
