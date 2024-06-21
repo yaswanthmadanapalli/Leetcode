6ab5dab1d9199ebab0790a14f4d88afb3dee9997
YASWANTH MADANAPALLI <71337774+yaswanthmadanapalli@users.noreply.github.com>
2024-06-21 15:44:48 -0700

    Time: 7 ms (74.71%), Space: 44.6 MB (15.19%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,22 +1,35 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        HashMap<Integer, Integer> hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int key=nums[i];
-            hm.put(key,hm.getOrDefault(key,0)+1);
-        }
-        int[] result=new int[nums.length];
-        int total;
-        List<Integer> key=new ArrayList<>(hm.keySet());
-        for(int j=0;j<nums.length;j++){
-            total=0;
-            for(int k=0;k<key.size();k++){
-                if(nums[j]>key.get(k)){
-                    total+=hm.get(key.get(k));
+//         HashMap<Integer, Integer> hm=new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             int key=nums[i];
+//             hm.put(key,hm.getOrDefault(key,0)+1);
+//         }
+//         int[] result=new int[nums.length];
+//         int total;
+//         List<Integer> key=new ArrayList<>(hm.keySet());
+//         for(int j=0;j<nums.length;j++){
+//             total=0;
+//             for(int k=0;k<key.size();k++){
+//                 if(nums[j]>key.get(k)){
+//                     total+=hm.get(key.get(k));
                     
+//                 }
+//             }
+//             result[j]=total;
+//         }
+//         return result;
+        int n=nums.length;
+        int[] result=new int[n];
+        int count;
+        for(int i=0;i<n;i++){
+            count=0;
+            for(int j=0;j<n;j++){
+                if(nums[i]>nums[j]){
+                    count++;
                 }
             }
-            result[j]=total;
+            result[i]=count;
         }
         return result;
     }
