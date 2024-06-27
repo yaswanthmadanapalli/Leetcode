ad6dbbf44895a6f55e32a393c6f704f00010900e
YASWANTH MADANAPALLI <71337774+yaswanthmadanapalli@users.noreply.github.com>
2024-06-27 09:56:11 -0700

    Time: 2 ms (99.04%), Space: 42.2 MB (99.46%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
         //String result=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        // if(s.toLowerCase.charAt(leftIndex)!=s.toLowerCase.charAt(rightIndex)){
+            //     return false;
+            // }
         int leftIndex=0;
         int rightIndex=s.length()-1;
         while(leftIndex<rightIndex){
-            while (leftIndex < rightIndex && !Character.isLetterOrDigit(s.charAt(leftIndex))) {
+            while (leftIndex<rightIndex && !Character.isLetterOrDigit(s.charAt(leftIndex))) {
                 leftIndex++;
             }
             // Find the next alphanumeric character from the right
-            while (leftIndex < rightIndex && !Character.isLetterOrDigit(s.charAt(rightIndex))) {
+            while (leftIndex<rightIndex && !Character.isLetterOrDigit(s.charAt(rightIndex))) {
                 rightIndex--;
             }
-            // if(s.toLowerCase.charAt(leftIndex)!=s.toLowerCase.charAt(rightIndex)){
-            //     return false;
-            // }
-            if (Character.toLowerCase(s.charAt(leftIndex)) != Character.toLowerCase(s.charAt(rightIndex))) {
+            
+            if(leftIndex<rightIndex && Character.toLowerCase(s.charAt(leftIndex))!=Character.toLowerCase(s.charAt(rightIndex)))        {
                 return false;
             }
             leftIndex++;
